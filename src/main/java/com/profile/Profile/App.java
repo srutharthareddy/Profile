2848f59c9f99e25a62c783fb8b9dbc66430ef413
srutharthareddy <41075785+srutharthareddy@users.noreply.github.com>
2018-11-10 15:28:43 -0600

    Update App.java

--- src/main/java/com/profile/Profile/App.java
+++ src/main/java/com/profile/Profile/App.java
@@ -6,8 +6,11 @@ package com.profile.Profile;
  */
 public class App 
 {
+    int a=10;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+    
+    
 }
